801ba86647c51f24339d5488d167eec0d9bdc2e8
ydhekim <yasin.d.hekim@gmail.com>
2018-11-26 12:33:17 +0300

    initial commit

--- stock-management/src/main/java/io/github/ydhekim/controller/LoginController.java
+++ stock-management/src/main/java/io/github/ydhekim/controller/LoginController.java
@@ -1,41 +1,28 @@
 package io.github.ydhekim.controller;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class LoginController
- */
 @WebServlet("/LoginController")
 public class LoginController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public LoginController() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		int employeeId = Integer.parseInt(request.getParameter("employee_id"));
+		int employeePw = Integer.parseInt(request.getParameter("employee_pw"));
+
+		if (employeeId == 1 && employeePw == 1) {
+			RequestDispatcher dispatcher = request.getRequestDispatcher("success.html");
+			dispatcher.forward(request, response);
+		}
 	}
 
 }
